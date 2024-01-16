b88dd75efdcd76e31ec5ed77f95343a7d44578a1
EliCer95 <89043035+EliCer95@users.noreply.github.com>
2024-01-16 14:08:44 +0000

    Removed 'unit' variable

--- src/script.js
+++ src/script.js
@@ -43,7 +43,6 @@ function formatTime(time) {
 }
 
 function getForecast(coordinates) {
-  let unit = "metric";
   let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
